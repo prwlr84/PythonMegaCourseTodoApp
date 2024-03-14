45f29483cbf53be7bcdff8eea869746e48d3cc16
Antal Bako <antal.bako@tradelink.co>
2024-03-14 12:28:27 +0100

    fix edit and complete, UI changes, error handling, image as button

--- gui.py
+++ gui.py
@@ -1,16 +1,23 @@
 from functions import *
 import PySimpleGUI as pysg
 
+pysg.theme('DarkPurple4')
+
 label = pysg.Text('Type in a to do')
 input_field = pysg.InputText(key='todo', tooltip='Enter to do')
-add_button = pysg.Button('Add')
+add_button = pysg.Button(image_source='add.png', button_color='purple',
+                         mouseover_colors='red', tooltip='button')
 edit_button = pysg.Button('Edit')
+complete_button = pysg.Button('Complete')
+exit_button = pysg.Button('Exit')
 display = pysg.Listbox(values=[t.strip('\n') for t in get_todos()], key='list',
                        enable_events=True, size=(45, 10))
+message = pysg.Text(key='message')
 
 layout = [[label],
           [input_field, add_button],
-          [display, edit_button]]
+          [display, edit_button, complete_button],
+          [exit_button, message]]
 
 window = pysg.Window('ToDoApp',
                      layout=layout,
@@ -26,14 +33,29 @@ while True:
                 todos.append(value['todo'] + '\n')
                 write_todos(todos)
             window['list'].update(values=[t.strip('\n') for t in todos])
-            print(todos)
         case 'Edit':
-            todos = get_todos()
-            todos[todos.index(value['list'][0] + '\n')] = value['todo'] + '\n'
-            write_todos(todos)
-            window['list'].update(values=[t.strip('\n') for t in todos])
+            try:
+                todos = get_todos()
+                todos[todos.index(value['list'][0] + '\n')] = value['todo'] + '\n'
+                write_todos(todos)
+                window['list'].update(values=[t.strip('\n') for t in todos])
+            except IndexError:
+                window['message'].update(value='Select an item first', text_color='red')
+                continue
+        case 'Complete':
+            try:
+                todos = get_todos()
+                todos.pop(todos.index(value['list'][0] + '\n'))
+                write_todos(todos)
+                window['list'].update(values=[t.strip('\n') for t in todos])
+                window['todo'].update(value='')
+            except IndexError:
+                pysg.Popup('Select an item first', font=('Helvetica', 20))
         case 'list':
             window['todo'].update(value=value['list'][0])
+            window['message'].update(value='')
+        case 'Exit':
+            break
         case pysg.WINDOW_CLOSED:
             break
 
